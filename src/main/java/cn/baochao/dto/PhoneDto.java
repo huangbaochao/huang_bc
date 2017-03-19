ae8f17a74d1c845ce9e62d35e996540bc2418cbd
huangbc <huangbc@dadi18.com>
2017-03-19 13:23:34 +0800

    初始化Phone6s

---
+++ src/main/java/cn/baochao/dto/PhoneDto.java
@@ -0,0 +1,37 @@
+package cn.baochao.dto;
+
+import java.io.Serializable;
+
+/**
+ * Created by huangbaochao on 2017/3/19 $version 1.0.
+ */
+public class PhoneDto implements Serializable{
+
+    private int id;
+
+    private String color;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "PhoneDto{" +
+                "id=" + id +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
