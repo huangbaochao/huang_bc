eb69b055e80da437ddff0db00d77cc0e4d2f47d9
huangbc <1062460526@qq.com>
2017-03-20 10:47:47 +0800

    add git命令

---
+++ src/main/java/cn/baochao/test/Demo1.java
@@ -0,0 +1,18 @@
+package cn.baochao.test;
+
+/**
+ * Created by huangbaochao on 2017/3/20 $version 1.0.
+ */
+public class Demo1 {
+
+    public static void main(String[] args) {
+
+        //git的常用命令
+        // git add .  , git commit -m  "" ,git commit -am "" //修改的文件提交的index and hard区
+        //git branch ,git status , git checkout baochao ,
+        //git reset --hard "commit id" ,git log --oneline ,git reflog //查看历史提交记录
+        //cat Phone.java 查看某个文件，
+        //
+    }
+
+}
