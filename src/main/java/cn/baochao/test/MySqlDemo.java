c7a179e912416c55e0957d33bda22711b649524a
huangbc <huangbc@dadi18.com>
2017-05-09 13:22:48 +0800

    mysql case 的使用

---
+++ src/main/java/cn/baochao/test/MySqlDemo.java
@@ -0,0 +1,50 @@
+package cn.baochao.test;
+
+/**
+ * Created by huangbc on 2017/5/3.
+ */
+public class MySqlDemo {
+
+    public static void main(String[] args) {
+
+        //-----------------------case 的使用----------------------------
+        //select user_id,user_name,age,
+//        case when user_name = '2' then 'yong'
+//        when user_name='5' then 'handsome'
+//        else 'just so so ' end
+//        from user;
+        //
+        //---------------------------------------------------------------------------
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+
+
+    }
+
+
+}
