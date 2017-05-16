ee05f6a3e4954a5023ee09c3d40f5d595bf28a6b
huangbc <huangbc@dadi18.com>
2017-05-16 08:46:35 +0800

    add Object方法

---
+++ src/main/java/cn/baochao/test/Demo10.java
@@ -0,0 +1,34 @@
+package cn.baochao.test;
+
+/**
+ * Created by huangbc on 2017/5/15.
+ */
+public class Demo10 {
+
+    private static final int MAXIMUM_CAPACITY = 1 << 3;
+
+    public static void main(String[] args) {
+        System.out.println(MAXIMUM_CAPACITY);
+        //-----------------------类加载器-------------------------------------------
+        //加载顺序：加载、验证、准备、初始化、卸载。5个阶段
+        //
+        //--------------------------intellij idea--------------------------------------------
+        //http://idea.imsxm.com/
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+    }
+
+}
